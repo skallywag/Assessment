e8ce83ef05538dd5a04490320f224afc3ddd0b1f
Samuel Lee <lee.sam1492@gmail.com>
2021-12-26 11:16:48 -0700

    working on booking ticket

--- client/JS/booking.js
+++ client/JS/booking.js
@@ -4,43 +4,103 @@ const bookForm = document.getElementById('booking-form')
 
 bookForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const city = document.getElementById('book-city').value
+    let city = document.getElementById('book-city').value
     // console.log(city);
-    const dateSlct = document.getElementById('date-slct').value
+    let dateSlct = document.getElementById('date-slct').value
     // console.log(dateSlct);
-    const weight = document.getElementById('payload').value
+    let weight = document.getElementById('payload').value
     // console.log(weight);
-    const ticket = document.getElementById('ticket-qty').value
+    let ticketQty = document.getElementById('ticket-qty').value
     // console.log(ticket);
     
-    if(bookValidate({city, weight, ticket, dateSlct})){
+    if(bookValidate({city, weight, ticketQty, dateSlct})){
         document.getElementById('book-city').value = 'City:'
         document.getElementById('payload').value = ''
         document.getElementById('ticket-qty').value = 'QTY'
         document.getElementById('date-slct').value = 'mm/dd/yyyy'
+
         let user = JSON.parse(localStorage.getItem('user'))
         let userId = user.id
 
-       
-
-        const card = `<h1>Your Ticket Info:</h1>
-                    <div>
-                    <span>Desination:</span> <h2 class="destination">${city}</h2>
-                    <span>Payload:</span> <h2 class="destination">${weight}</h2>
-                    <span>Ticket QTY:</span> <h2 class="destination">${ticket}</h2>
-                    <span>Date:</span> <h2 class="destination">${dateSlct}</h2>
-                     <button id="confirm-book-btn">Book</button>
-                     </div>
+        const card = `
+                <h1 class="bk-confirm-title">Your Ticket Info:</h1>
+                    <div class="confirm-ticket">
+                        <div class="book-info">
+                          <span>Name:</span><h2 id="updateName" class="update">${user.name}</h2>
+                        </div>
+                        <div class="book-info">
+                           <span>Email:</span> <h2 id="updateEmail">${user.email}</h2>
+                        </div>
+                        <div class="book-info">
+                            <span>Desination:</span> <h2 id="updateCity" class="update">${city}</h2>
+                        </div>
+                        <div class="book-info">
+                            <span>Payload:</span> <h2 id="updateWeight" class="update">${weight}</h2>
+                        </div> 
+                        <div class="book-info">
+                            <span>Ticket QTY:</span> <h2  id="updateTicketQty" class="update">${ticketQty}</h2>
+                        </div>   
+                        <div class="book-info">
+                            <span>Date:</span> <h2  id="dateUpdate" class="update">${dateSlct}</h2> 
+                        </div>
+                        <div class="price-sum">
+                            <span>Price:</span><div id="price"> </div><span>USD</span>
+                        </div>
+                    </div>
+                    <div class="edit-btns">
+                        <button id="edit-book-btn">Edit</button>
+                        <button id="done-book-btn">Done</button>
+                        <button id="delete-bk-btn">X</button>
+                    </div> 
+                    <div class="book-btn">
+                        <button id="confirm-book-btn">Book</button>
+                    </div>
         `
-        document.getElementById('confirm-con').innerHTML = card
-        
+       const confrmCon = document.getElementById('confirm-con')
+       confrmCon.innerHTML = card
 
-        
-        
+       if(confrmCon.style.display === 'none'){
+            confrmCon.style.display = 'flex'
+       }
+
+       const editBtn = document.getElementById('edit-book-btn')
+       const doneBtn = document.getElementById('done-book-btn')
+    //    const emailUpdate = document.getElementById('')
+    //    const updateName = document.getElementById('')
+       const updateInfo = document.querySelectorAll('.update')
+    //    const updatePrice = document.getElementById('price')
+       
+       editBtn.addEventListener('click', () => {    
+           for(let i = 0; i < updateInfo.length; i++){
+               updateInfo[i].contentEditable = 'true'
+               updateInfo[i].style.backgroundColor = 'darkgray'
+           }
+       })
+
+       doneBtn.addEventListener('click', () => {
+
+           for(let i = 0; i < updateInfo.length; i++){
+               updateInfo[i].contentEditable = 'false'
+               updateInfo[i].style.backgroundColor = '#C1440D'
+           }
+
+         
+
+          
+       })
+
+       const deleteBtn = document.getElementById('delete-bk-btn')
+       deleteBtn.addEventListener('click', () => {
+        document.getElementById('confirm-con').remove()
+       })
+
         const bookBtn = document.getElementById('confirm-book-btn')
-        console.log(bookBtn);
+        // console.log(bookBtn);
         bookBtn.addEventListener('click', () => {
-            axios.post('http://localhost:5432/api/trips', {userId, city, weight, ticket, dateSlct})
+            if(confrmCon.style.display === 'flex'){
+                confrmCon.style.display = 'none'
+            }
+            axios.post('http://localhost:5432/api/trips', {userId, city, weight, ticketQty, dateSlct})
             .then(res => {
                 console.log(res)
             })        
@@ -61,14 +121,14 @@ function bookValidate(input){
     
     if(input.weight === 0 || input.weight === ''){
         console.log(errors.weight);
-        errors.weight = `<span>please eanter a payload</span>`
+        errors.weight = `<span>please enter a payload/valid</span>`
         document.getElementById('payloadErr').innerHTML = errors.weight
     }
     
-    if(input.ticket == 'QTY'){
-        console.log(errors.ticket);
-        errors.ticket = `<span>please select qty</span>`
-        document.getElementById('qtyErr').innerHTML = errors.ticket
+    if(input.ticketQty == 'QTY'){
+        console.log(errors.ticketQty);
+        errors.ticketQty = `<span>please select qty</span>`
+        document.getElementById('qtyErr').innerHTML = errors.ticketQty
     }
 
     if(input.dateSlct == 'mm/dd/yyyy'){
@@ -81,6 +141,9 @@ function bookValidate(input){
        const logReq = document.getElementById('login-required')
        logReq.innerHTML = `Please Login`
     }
+
+    
+
 
     
     if(Object.keys(errors).length === 0) return true
