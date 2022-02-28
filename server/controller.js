e659427ef19b067722eafbd383cda50315555f0b
Samuel Lee <lee.sam1492@gmail.com>
2022-02-28 15:51:16 -0700

    re-deploying

--- server/controller.js
+++ server/controller.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
-const { CONNECTION_STRING } = process.env;
+const { DATABASE_URL } = process.env;
 const res = require("express/lib/response");
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(CONNECTION_STRING, {
+const sequelize = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
     ssl: {
